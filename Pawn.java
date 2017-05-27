0600c53267fbbbeb59b7dba85e210171d44129c1
Kysnn <crhah.rmrtr@gmail.com>
2017-05-27 11:20:09 +0300

    Pawn Fix

--- Pawn.java
+++ Pawn.java
@@ -1,4 +1,4 @@
-package sinanchess12apr;
+
 
 import java.awt.Color;
 import java.util.ArrayList;
@@ -14,62 +14,83 @@ public class Pawn extends Piece {
     }
 
     @Override
-    void calculateWhereCanItGo() {
-    	availablePos.clear();
-    	if (cB.tableAsSquare[x][y].onThis.side.equals("white")) {      //WhitePawn
+   void calculateWhereCanItGo() {
+     availablePos.clear();
+        if (cB.tableAsSquare[x][y].onThis.side.equals("white")) {      //WhitePawn
             if (cB.tableAsSquare[x][y].onThis.x == 6) {
                 if (cB.tableAsSquare[x - 1][y].onThis == null) { //FirstMove
                     availablePos.add(cB.tableAsSquare[x - 1][y]);
                     if (cB.tableAsSquare[x - 2][y].onThis == null) {
-                    	availablePos.add(cB.tableAsSquare[x - 2][y]);
+                        availablePos.add(cB.tableAsSquare[x - 2][y]);
                     }
                 }
+                if (y != 7) {
+                    if (cB.tableAsSquare[x - 1][y + 1].onThis != null && !cB.tableAsSquare[x - 1][y + 1].onThis.side.equals("white")) {
+                        availablePos.add(cB.tableAsSquare[x - 1][y + 1]);
+                    }
+                }
+                if (y != 0) {
+                    if (cB.tableAsSquare[x - 1][y - 1].onThis != null && !cB.tableAsSquare[x - 1][y - 1].onThis.side.equals("white")) {
+                        availablePos.add(cB.tableAsSquare[x - 1][y - 1]);
+                    }
+                }
+
             } else if (x != 0) {
                 if (cB.tableAsSquare[x - 1][y].onThis == null) {
-                	 availablePos.add(cB.tableAsSquare[x - 1][y]);
+                    availablePos.add(cB.tableAsSquare[x - 1][y]);
                 }
-                
+
                 if (y != 7) {
-                    if (cB.tableAsSquare[x - 1][y+1].onThis != null) {
-                    	 availablePos.add(cB.tableAsSquare[x - 1][y+1]);
+                    if (cB.tableAsSquare[x - 1][y + 1].onThis != null && !cB.tableAsSquare[x - 1][y + 1].onThis.side.equals("white")) {
+                        availablePos.add(cB.tableAsSquare[x - 1][y + 1]);
                     }
                 }
-                   if (y != 0) {
-                    if (cB.tableAsSquare[x - 1][y-1].onThis != null) {
-                    	availablePos.add(cB.tableAsSquare[x - 1][y-1]);
+                if (y != 0) {
+                    if (cB.tableAsSquare[x - 1][y - 1].onThis != null && !cB.tableAsSquare[x - 1][y - 1].onThis.side.equals("white")) {
+                        availablePos.add(cB.tableAsSquare[x - 1][y - 1]);
                     }
                 }
-                
+
             }
         } else {                                                       //BlackPawn
             if (cB.tableAsSquare[x][y].onThis.x == 1) {
                 if (cB.tableAsSquare[x + 1][y].onThis == null) { //FirtMove
-                	availablePos.add(cB.tableAsSquare[x + 1][y]);
+                    availablePos.add(cB.tableAsSquare[x + 1][y]);
                     if (cB.tableAsSquare[x + 2][y].onThis == null) {
-                    	availablePos.add(cB.tableAsSquare[x + 2][y]);
+                        availablePos.add(cB.tableAsSquare[x + 2][y]);
+                    }
+                }
+                if (y != 7) {
+                    if (cB.tableAsSquare[x + 1][y + 1].onThis != null && !cB.tableAsSquare[x + 1][y + 1].onThis.side.equals("black")) {
+                        availablePos.add(cB.tableAsSquare[x + 1][y + 1]);
+                    }
+                }
+                if (y != 0) {
+                    if (cB.tableAsSquare[x + 1][y - 1].onThis != null && !cB.tableAsSquare[x + 1][y - 1].onThis.side.equals("black")) {
+                        availablePos.add(cB.tableAsSquare[x + 1][y - 1]);
                     }
                 }
 
             } else if (x != 7) {
                 if (cB.tableAsSquare[x + 1][y].onThis == null) {
-                	availablePos.add(cB.tableAsSquare[x + 1][y]);
+                    availablePos.add(cB.tableAsSquare[x + 1][y]);
                 }
                 if (y != 7) {
-                    if (cB.tableAsSquare[x + 1][y+1].onThis != null) {
-                    	availablePos.add(cB.tableAsSquare[x + 1][y+1]);
+                    if (cB.tableAsSquare[x + 1][y + 1].onThis != null && !cB.tableAsSquare[x + 1][y + 1].onThis.side.equals("black")) {
+                        availablePos.add(cB.tableAsSquare[x + 1][y + 1]);
                     }
                 }
-                   if (y != 0) {
-                    if (cB.tableAsSquare[x + 1][y-1].onThis != null) {
-                    	availablePos.add(cB.tableAsSquare[x + 1][y-1]);
+                if (y != 0) {
+                    if (cB.tableAsSquare[x + 1][y - 1].onThis != null && !cB.tableAsSquare[x + 1][y - 1].onThis.side.equals("black")) {
+                        availablePos.add(cB.tableAsSquare[x + 1][y - 1]);
                     }
                 }
 
             }
         }
 
-
     }
+
 
     @Override
     void move() {
